187d8bb319c6d9702b0615957432602e643d1c3c
Ujjwal Vij <uvij027@gmail.com>
2023-10-09 14:07:47 +0530

    push

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -3,6 +3,11 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -12,7 +17,6 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const router = useRouter();
 
-  
   const handleLogout = () => {
     //Handle LogOut event
   };
@@ -30,8 +34,47 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
       )}
     >
       <div className="w-full mb-4 flex items-center justify-between">
-        <div className="hidden md:flex gap-x-2 items-center">Hello World</div>
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.back()}
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.forward()}
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Log in
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
