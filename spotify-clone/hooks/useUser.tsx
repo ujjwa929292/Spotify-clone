62c0870256119b453cb91aa99cf89a42ffe6666c
Ujjwal Vij <uvij027@gmail.com>
2023-10-11 11:47:18 +0530

    push

--- spotify-clone/hooks/useUser.tsx
+++ spotify-clone/hooks/useUser.tsx
@@ -4,7 +4,7 @@ import {
   useSessionContext,
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react";
-import { createContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type UserContextType = {
   accessToken: string | null;
@@ -14,7 +14,7 @@ type UserContextType = {
   subscription: Subscription | null;
 };
 
-export const UserContextType = createContext<UserContextType | undefined>(
+export const UserContext = createContext<UserContextType | undefined>(
   undefined
 );
 
@@ -33,4 +33,52 @@ export const MyUserContextProvider = (props: Props) => {
   const [isLoadingData, setIsLoadingData] = useState(false);
   const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
   const [subscription, setSubscription] = useState<Subscription | null>(null);
+
+  const getUserDetails = () => superbase.from('users').select('*').single();
+  const getSubscription = () => superbase.from('subscriptions').select('*, prices(*, products(*))').in('status', ['trialing', 'active']).single();
+
+  useEffect(() => {
+    if(user && !isLoadingData && !userDetails && !subscription){
+        setIsLoadingData(true);
+
+        Promise.allSettled([getUserDetails(), getSubscription()]).then(
+         (results) => {
+           const userDetailsPromise = results[0];
+           const subscriptionPromise = results[1];
+
+           if(userDetailsPromise.status === "fulfilled"){
+            setUserDetails(userDetailsPromise.value.data as UserDetails);
+           }
+
+           if(subscriptionPromise.status === "fulfilled") {
+            setSubscription(subscriptionPromise.value.data as Subscription);
+           }
+
+           setIsLoadingData(false);
+         }   
+        );
+    } else if (!user && !isLoadingUser && !isLoadingData){
+        setUserDetails(null);
+        setSubscription(null);
+    }
+  },[user, isLoadingUser]);
+
+  const value = {
+   accessToken,
+   user,
+   userDetails,
+   isLoading: isLoadingUser || isLoadingData,
+   subscription
+  }
+
+  return <UserContext.Provider value={value} {...props} />
 };
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a MyUserContextProvider');
+    }
+
+    return context;
+}
