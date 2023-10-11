b0ba757a8f2aa9de0248e425a3692c4061c077e8
Ujjwal Vij <uvij027@gmail.com>
2023-10-11 17:28:57 +0530

    push

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import SuperbaseProvider from "@/providers/SuperbaseProvider";
+import UserProvider from "@/hooks/useProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -20,7 +22,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <SuperbaseProvider>
-          <Sidebar>{children}</Sidebar>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
         </SuperbaseProvider>
       </body>
     </html>
